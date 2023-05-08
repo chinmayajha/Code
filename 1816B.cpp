8fbc04c9ef00251cfd3209e7dde53eb5aedd058b
chinmayajha <84258488+chinmayajha@users.noreply.github.com>
2023-05-08 05:43:41 +0530

    Update 1816B.cpp

--- 1816B.cpp
+++ 1816B.cpp
@@ -7,21 +7,10 @@ signed main() {
     cin >> t;
     while (t--) {
         int n; cin >> n;
-        vector<int> a(2 * n);
-        int x = 2 * n;
-        a[2 * n - 1] = x--;
-        a[0] = x--;
-
-        // filling all blue boxes
-        for(int i = 2; i < n; i += 2) a[i] = x--;
-        for(int i = n + 1; i < 2 * n - 1; i += 2) a[i] = x--;
-        // filling all red boxes
-        for(int i = 1; i < n; i += 2) a[i] = (i % n) + 1;
-        for(int i = n; i < 2 * n - 1; i += 2) a[i] = (i % n) + 1;
-
-        for(int i = 0; i < n; ++i) cout << a[i] << " ";
-        cout << "\n";
-        for(int i = n; i < 2 * n; ++i) cout << a[i] << " ";
-        cout << "\n";
+        string s; 
+        getline(cin, s);
+        
     }
 }
+
+
