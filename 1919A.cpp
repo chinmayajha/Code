6708219960416bf6f1f4e7794d25b437d0b71ad6
Pusher <pusher@gmail.com>
2024-03-14 13:16:04 +0530

    .

---
+++ 1919A.cpp
@@ -0,0 +1,14 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define int long long
+
+signed main() {
+    mt19937 rng(chrono::steady_clock::now().time_since_epoch().count()); // for shuffle(..., ..., rng)
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); int T = 1;
+    cin >> T;
+    while (T--) {
+        int n, m; cin >> n >> m;
+        cout << ((n + m) % 2 == 0 ? "Bob\n" : "Alice\n"); 
+    }
+}
+
